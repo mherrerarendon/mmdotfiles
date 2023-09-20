7c46168452006f03dbad42a3a0669f3d218e1c4b
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2023-09-20 16:44:01 -0600

    added more steps to the setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 MHROOT="$HOME/Developer/mhgit"
 MMROOT="$HOME/Developer/mmgit"
 
+# General setup
 git clone mmdotfiles ~/Developer/mmdotfiles
+# install https://ohmyz.sh/
+rm .zshrc
+ln -s /Users/mherrerarendon/Developer/mmdotfiles/zshrc.sh ~/.zshrc
+ln -s /Users/mherrerarendon/Developer/mmdotfiles/gitconfig ./.gitconfig
+echo "export PATH=\"$HOME/bin:$PATH\"" >> ~/.zshenv
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -11,16 +17,17 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # vim
 brew install neovim
-ln -s $HOME/Developer/dotfiles/snippets $HOME/Library/Application\ Support/Code/User/snippets
-rm .zshrc
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/zshrc.sh ~/.zshrc
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/gitconfig ./.gitconfig
-rm ~/Library/Application\ Support/Code/User/settings.json
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/code.json ~/Library/Application\ Support/Code/User/settings.json
-
 mkdir .vim
 ln -s /Users/mherrerarendon/Developer/mmdotfiles/vimrc ~/.vim/vimrc
-
 mkdir .config
 mkdir ~/.config/nvim
 ln -s /Users/mherrerarendon/Developer/mmdotfiles/init.vim ~/.config/nvim/init.vim
+
+# vscode
+ln -s $HOME/Developer/dotfiles/snippets $HOME/Library/Application\ Support/Code/User/snippets
+rm ~/Library/Application\ Support/Code/User/settings.json
+ln -s /Users/mherrerarendon/Developer/mmdotfiles/code.json ~/Library/Application\ Support/Code/User/settings.json
+# install the `code` command line util in ~/bin
+
+# sublime merge
+ln -s /Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge /Users/mherrerarendon/bin/smerge
