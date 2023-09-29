18ccfa6d3589dddfe0899b7f643ae18988f36222
Marco Herrera-Rendon <mherrerarendon@Marcos-Mac-mini.Starry-inc.net>
2023-09-29 15:48:32 -0600

    make it work on mini

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 # Global Constants
 MHROOT="$HOME/Developer/mhgit"
 MMROOT="$HOME/Developer/mmgit"
+DEVELOPER="/Volumes/Rendon/RDeveloper"
+DOTFILES="$DEVELOPER/mmdotfiles"
 
 # General setup
-git clone mmdotfiles ~/Developer/mmdotfiles
+git clone mmdotfiles $DOTFILES
 # install https://ohmyz.sh/
-rm .zshrc
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/zshrc.sh ~/.zshrc
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/gitconfig ./.gitconfig
-echo "export PATH=\"$HOME/bin:$PATH\"" >> ~/.zshenv
+rm $HOME/.zshrc
+ln -s $DOTFILES/zshrc.sh $HOME/.zshrc
+ln -s $DOTFILES/gitconfig $HOME/.gitconfig
+echo "export PATH=\"$HOME/bin:$PATH\"" >> $HOME/.zshenv
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -18,15 +20,15 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # vim
 brew install neovim
 mkdir .vim
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/vimrc ~/.vim/vimrc
+ln -s $DOTFILES/vimrc $HOME/.vim/vimrc
 mkdir .config
-mkdir ~/.config/nvim
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/init.vim ~/.config/nvim/init.vim
+mkdir $HOME/.config/nvim
+ln -s $DOTFILES/init.vim $HOME/.config/nvim/init.vim
 
 # vscode
 ln -s $HOME/Developer/dotfiles/snippets $HOME/Library/Application\ Support/Code/User/snippets
-rm ~/Library/Application\ Support/Code/User/settings.json
-ln -s /Users/mherrerarendon/Developer/mmdotfiles/code.json ~/Library/Application\ Support/Code/User/settings.json
+rm $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $DOTFILES/code.json $HOME/Library/Application\ Support/Code/User/settings.json
 # install the `code` command line util in ~/bin
 
 # sublime merge
