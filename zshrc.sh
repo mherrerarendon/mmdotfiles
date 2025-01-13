80e52521910db06cd42768ca4b802eb6411dfa3d
Marco Herrera-Rendon <mherrerarendon@gmail.com>
2025-01-13 10:25:07 -0700

    cleanup zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -1,11 +1,10 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
-
-source ~/Developer/mmdotfiles/alias.sh
 
 # Path to your oh-my-zsh installation.
-export ZSH="/Users/mherrerarendon/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
+
+source ~/Developer/git/dotfiles/alias.sh
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -73,7 +72,7 @@ ZSH_THEME="Eastwood"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-autosuggestions web-search copypath macos)
+plugins=(git zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -102,38 +101,3 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-export LESS="-F -E -X $LESS"
-
-export PATH="/usr/local/opt/ruby/bin:$PATH"
-
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Added by Amplify CLI binary installer
-export PATH="$HOME/.amplify/bin:$PATH"
-eval "$(rbenv init -)"
-export AWS_DEFAULT_REGION="us-east-1"
-export AWS_REGION="$AWS_DEFAULT_REGION"
-export AWS_PROFILE="makemusic"
-export DOCKER_DEFAULT_PLATFORM=linux/arm64
-alias ghcrlogin='echo "$GITHUB_PACKAGE_TOKEN" | docker login ghcr.io -u USERNAME --password-stdin'
-alias clockfix="sudo hwclock -s"
-alias dockerloginfix="rm ~/.docker/config.json"
-. $(eval echo $MMC_LOCATION/utils.sh)
-# BEGIN ANSIBLE MANAGED BLOCK
-# starts the SSH agent if it's not already running and adds the key to it to allow hitting github
-if [ -z "$SSH_AUTH_SOCK" ]; then
-   # Check for a currently running instance of the agent
-   RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
-   if [ "$RUNNING_AGENT" = "0" ]; then
-        # Launch a new instance of the agent
-        ssh-agent -s &> $HOME/.ssh/ssh-agent
-   fi
-   eval `cat $HOME/.ssh/ssh-agent`
-fi
-
-# END ANSIBLE MANAGED BLOCK
